891069979e6aa38fd2f4cca18eab4723f52115cb
DOGUK <dgkim1994@gmail.com>
2020-07-10 16:07:51 +0900

    update

--- movie_2020/src/App.js
+++ movie_2020/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import PropType from 'prop-types';
 
-function PictureList(props) {
-  console.log(props)
-}
-
-function App() {
-  return (
-    <div>
-      <pictureList />
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+  add = () => {
+    this.setState( current => ({count: current.count + 1}))
+  };
+  minus = () => {
+    this.setState( current => ({count: current.count - 1}))
+  }
+  render(){
+    return <div>
+      <h1>The num is: {this.state.count}</h1>
+      <button onClick={ this.add } >add</button>
+      <button onClick={ this.minus } >minus</button>
     </div>
-  );
+  }
 }
 
 export default App;
